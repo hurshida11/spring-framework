620de7ea84240387cade55cbc14e27bd4974d689
K D <6hurshi@gmail.com>
2022-02-23 20:35:48 -0600

    Abstraction

--- Spring-04-Abstraction/src/main/java/com/cydeo/repository/DBCommentRepository.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/repository/DBCommentRepository.java
@@ -1,9 +1,10 @@
 package com.cydeo.repository;
 
 import com.cydeo.model.Comment;
+import org.springframework.stereotype.Component;
 
 
-
+@Component
 public class DBCommentRepository implements CommentRepository{
     @Override
     public void storeComment(Comment comment) {
