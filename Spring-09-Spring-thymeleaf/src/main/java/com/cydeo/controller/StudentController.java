89167cdbb812860f2c516fe8ef91a060f1b2addc
K D <6hurshi@gmail.com>
2022-03-20 11:16:53 -0500

    Thymeleaf

--- Spring-09-Spring-thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -16,4 +16,9 @@ public class StudentController {
 
         return "student/register";
     }
+    @RequestMapping("/welcome")
+    public String info(){
+
+        return "student/welcome";
+    }
 }
