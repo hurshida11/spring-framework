8d04f3081a9290666b39ebd9932299377914587f
K D <6hurshi@gmail.com>
2022-04-26 13:02:09 -0500

    One to one relationship

---
+++ Spring-11-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,44 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+public class Employee extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "Date")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    //@OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    @JoinColumn(name="department_id")
+    private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
