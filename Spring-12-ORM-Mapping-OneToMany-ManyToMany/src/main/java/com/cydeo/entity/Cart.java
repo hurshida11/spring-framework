d348f733eea9b978cab14aea4435f482d5dc5cfe
K D <6hurshi@gmail.com>
2022-04-27 15:43:11 -0500

    Many To Many Relationship

--- Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/Cart.java
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/Cart.java
@@ -17,5 +17,6 @@ public class Cart {
 
 
     @ManyToMany
+    @JoinTable(name = "cart_item_rel",joinColumns = @JoinColumn(name = "c_id"),inverseJoinColumns =@JoinColumn(name = "i_id") )
     private List<Item> itemList;
 }
