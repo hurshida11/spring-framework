ed4ec2630f0f4bdfbe3500b6117a05eb4c3151e4
K D <6hurshi@gmail.com>
2022-04-30 10:10:06 -0500

    Cinema Lab

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/UserAccount.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_account")
+public class User extends BaseEntity{
+
+    private String email;
+    private String password;
+    private String username;
+
+    @OneToOne
+    private AccountDetails accountDetails;
+}
