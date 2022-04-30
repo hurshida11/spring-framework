3d824f505272a744c2d5a2382027713b146ea01c
K D <6hurshi@gmail.com>
2022-04-30 10:46:59 -0500

    Cinema Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/User.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/User.java
@@ -4,19 +4,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "user_account")
 public class User extends BaseEntity{
 
     private String email;
     private String password;
     private String username;
 
-    @OneToOne
-    private AccountDetails accountDetails;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_details_id")
+    private Account account;
 }
